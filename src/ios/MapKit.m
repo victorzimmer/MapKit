8aa5763cceac745bc2817ca21a7d45a399300cb3
Victor Zimmer <victor.zimmer@me.com>
2015-11-09 03:48:52 +0100

    More testing

--- src/ios/MapKit.m
+++ src/ios/MapKit.m
@@ -24,4 +24,22 @@
     [self success:result callbackId:callbackId];
 }
 
+
+- (void)createMapView:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    int height = [[command arguments] objectAtIndex:0];
+    int width = [[command arguments] objectAtIndex:1];
+    int xPos = [[command arguments] objectAtIndex:2];
+    int yPos = [[command arguments] objectAtIndex:3];
+    
+    int msg = xPos + yPos;
+    
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:msg];
+    
+    [self success:result callbackId:callbackId];
+}
+
 @end
