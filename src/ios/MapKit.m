3ed7bd3a9c95c142053ae8ab621c17dca939539d
Victor Zimmer <victor.zimmer@me.com>
2015-11-09 03:30:31 +0100

    Leaving Swift for ObjC

---
+++ src/ios/MapKit.m
@@ -0,0 +1,9 @@
+//
+//  MapKit.m
+//  
+//
+//  Created by Victor Zimmer on 09/11/15.
+//
+//
+
+#import <Foundation/Foundation.h>
--- src/ios/cordova-plugin-mapkit-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  cordova-plugin-mapkit-Bridging-Header.h
-//  
-//
-//  Created by Victor Zimmer on 09/11/15.
-//
-//
-
-#import <Cordova/CDV.h>
