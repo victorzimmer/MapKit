7f24d3701ab004b9024773f9df9f361e9e6d7042
Victor Zimmer <victor.zimmer@me.com>
2015-11-10 05:19:20 +0100

    TempFix

--- src/ios/MapKit.h
+++ src/ios/MapKit.h
@@ -12,29 +12,29 @@
 
 - (void) test:(CDVInvokedUrlCommand*)command;
 
-- (void) checkLocationPermission(CDVInvokedUrlCommand*)command;
-- (void) requestLocationAlwaysPermission(CDVInvokedUrlCommand*)command;
-- (void) requestLocationWhenInUsePermission(CDVInvokedUrlCommand*)command;
-
-- (void) createMapView(CDVInvokedUrlCommand*)command;
-- (void) removeMapView(CDVInvokedUrlCommand*)command;
-
-- (void) showMapScale(CDVInvokedUrlCommand*)command;
-- (void) hideMapScale(CDVInvokedUrlCommand*)command;
-
-- (void) showMapCompass(CDVInvokedUrlCommand*)command;
-- (void) hideMapCompass(CDVInvokedUrlCommand*)command;
-
-- (void) showMapTraffic(CDVInvokedUrlCommand*)command;
-- (void) hideMapTraffic(CDVInvokedUrlCommand*)command;
-
-- (void) showMapBuildings(CDVInvokedUrlCommand*)command;
-- (void) hideMapBuildings(CDVInvokedUrlCommand*)command;
-
-- (void) showMapUserLocation(CDVInvokedUrlCommand*)command;
-- (void) hideMapUserLocation(CDVInvokedUrlCommand*)command;
-
-- (void) showMapPointsOfInterest(CDVInvokedUrlCommand*)command;
-- (void) hideMapPointsOfInterest(CDVInvokedUrlCommand*)command;
+// - (void) checkLocationPermission(CDVInvokedUrlCommand*)command;
+// - (void) requestLocationAlwaysPermission(CDVInvokedUrlCommand*)command;
+// - (void) requestLocationWhenInUsePermission(CDVInvokedUrlCommand*)command;
+//
+// - (void) createMapView(CDVInvokedUrlCommand*)command;
+// - (void) removeMapView(CDVInvokedUrlCommand*)command;
+//
+// - (void) showMapScale(CDVInvokedUrlCommand*)command;
+// - (void) hideMapScale(CDVInvokedUrlCommand*)command;
+//
+// - (void) showMapCompass(CDVInvokedUrlCommand*)command;
+// - (void) hideMapCompass(CDVInvokedUrlCommand*)command;
+//
+// - (void) showMapTraffic(CDVInvokedUrlCommand*)command;
+// - (void) hideMapTraffic(CDVInvokedUrlCommand*)command;
+//
+// - (void) showMapBuildings(CDVInvokedUrlCommand*)command;
+// - (void) hideMapBuildings(CDVInvokedUrlCommand*)command;
+//
+// - (void) showMapUserLocation(CDVInvokedUrlCommand*)command;
+// - (void) hideMapUserLocation(CDVInvokedUrlCommand*)command;
+//
+// - (void) showMapPointsOfInterest(CDVInvokedUrlCommand*)command;
+// - (void) hideMapPointsOfInterest(CDVInvokedUrlCommand*)command;
 
 @end
