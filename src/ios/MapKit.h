732d2f93db65a913b578b74076a6d8c93eacf3fd
Victor Zimmer <victor.zimmer@me.com>
2015-11-10 04:32:51 +0100

    Added most functions to ObjC part

--- src/ios/MapKit.h
+++ src/ios/MapKit.h
@@ -1,6 +1,6 @@
 //
 //  MapKit.h
-//  
+//
 //
 //  Created by Victor Zimmer on 09/11/15.
 //
@@ -11,5 +11,30 @@
 @interface MapKit : CDVPlugin
 
 - (void) test:(CDVInvokedUrlCommand*)command;
+
+- (void) checkLocationPermission(CDVInvokedUrlCommand*)command;
+- (void) requestLocationAlwaysPermission(CDVInvokedUrlCommand*)command;
+- (void) requestLocationWhenInUsePermission(CDVInvokedUrlCommand*)command;
+
+- (void) createMapView(CDVInvokedUrlCommand*)command;
+- (void) removeMapView(CDVInvokedUrlCommand*)command;
+
+- (void) showMapScale(CDVInvokedUrlCommand*)command;
+- (void) hideMapScale(CDVInvokedUrlCommand*)command;
+
+- (void) showMapCompass(CDVInvokedUrlCommand*)command;
+- (void) hideMapCompass(CDVInvokedUrlCommand*)command;
+
+- (void) showMapTraffic(CDVInvokedUrlCommand*)command;
+- (void) hideMapTraffic(CDVInvokedUrlCommand*)command;
+
+- (void) showMapBuildings(CDVInvokedUrlCommand*)command;
+- (void) hideMapBuildings(CDVInvokedUrlCommand*)command;
+
+- (void) showMapUserLocation(CDVInvokedUrlCommand*)command;
+- (void) hideMapUserLocation(CDVInvokedUrlCommand*)command;
+
+- (void) showMapPointsOfInterest(CDVInvokedUrlCommand*)command;
+- (void) hideMapPointsOfInterest(CDVInvokedUrlCommand*)command;
 
 @end
--- src/ios/MapKit.m
+++ src/ios/MapKit.m
@@ -1,45 +1,390 @@
 //
 //  MapKit.m
-//  
+//
 //
 //  Created by Victor Zimmer on 09/11/15.
 //
 //
 
 #import "MapKit.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+//@interface ViewController : UIViewController <MKMapViewDelegate>
 
 @implementation MapKit
 
+CLLocationManager* locationManager;
+
+- (id)init
+{
+
+}
+
 - (void)test:(CDVInvokedUrlCommand*)command
 {
-    
+
     NSString* callbackId = [command callbackId];
     NSString* name = [[command arguments] objectAtIndex:0];
     NSString* msg = [NSString stringWithFormat: @"MapKit, %@", name];
-    
+
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsString:msg];
-    
+
+
+
     [self success:result callbackId:callbackId];
+}
+
+- (void)checkLocationPermission:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+
+    CLAuthorizationStatus* authStatus = [CLLocationManager authorizationStatus];
+
+    NSString* resultString;
+
+    if (authStatus == kCLAuthorizationStatusAuthorized) {
+        resultString = @"LOCATION_AUTH_AUTHORIZED";
+    }
+    else if (authStatus == kCLAuthorizationStatusAuthorizedAlways) {
+        resultString = @"LOCATION_AUTH_AUTHORIZED_ALWAYS";
+    }
+    else if (authStatus == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        resultString = @"LOCATION_AUTH_AUTHORIZED_WHEN_IN_USE";
+    }
+    else if (authStatus == kCLAuthorizationStatusNotDetermined) {
+        resultString = @"LOCATION_AUTH_NOT_DETERMINED";
+    }
+    else if (authStatus == kCLAuthorizationStatusRestricted) {
+        resultString = @"LOCATION_AUTH_RESTRICTED";
+    }
+    else if (authStatus == kCLAuthorizationStatusDenied) {
+        resultString = @"LOCATION_AUTH_DENIED";
+    }
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:resultString];
+
+
+
+    [self success:result callbackId:callbackId];
+}
+
+- (void)requestLocationWhenInUsePermission:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    [locationManager requestWhenInUseAuthorization];
+    [locationManager startUpdatingLocation];
+
+
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:@"OK"];
+
+    [self success:result callbackId:callbackId];
+}
+
+
+- (void)requestLocationAlwaysPermission:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    [locationManager requestAlwaysAuthorization];
+    [locationManager startUpdatingLocation];
+
+
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:@"OK"];
+
+    [self success:result callbackId:callbackId];
+
 }
 
 
 - (void)createMapView:(CDVInvokedUrlCommand*)command
 {
     NSString* callbackId = [command callbackId];
-    int height = [[command arguments] objectAtIndex:0];
-    int width = [[command arguments] objectAtIndex:1];
-    int xPos = [[command arguments] objectAtIndex:2];
-    int yPos = [[command arguments] objectAtIndex:3];
-    
-    int msg = xPos + yPos;
-    
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    CGFloat height = [[[command arguments] objectAtIndex:1]floatValue];
+    CGFloat width = [[[command arguments] objectAtIndex:2]floatValue];
+    CGFloat xPos = [[[command arguments] objectAtIndex:3]floatValue];
+    CGFloat yPos = [[[command arguments] objectAtIndex:4]floatValue];
+
+    MKMapView* mapView = [[MKMapView alloc]initWithFrame:CGRectMake(xPos, yPos, width, height)];
+    mapView.tag = mapId;
+    mapView.delegate = self;
+    [self.webView addSubview:mapView];
+
+
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
-    
+                               messageAsString:mapId];
+
     [self success:result callbackId:callbackId];
 }
+
+
+- (void)removeMapView:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+    [mapView removeFromSuperview];
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)isShowingUserLocation:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    NSString* stringRes;
+
+    if (mapView.userLocationVisible) {
+        stringRes = @"true";
+    }
+    else
+    {
+        stringRes = @"false";
+    }
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:stringRes];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+
+- (void)showMapScale:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsScale = YES;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)hideMapScale:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsScale = NO;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)showMapUserLocation:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsUserLocation = YES;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)hideMapUserLocation:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsUserLocation = NO;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)showMapCompass:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsCompass = YES;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)hideMapCompass:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsCompass = NO;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)showMapPointsOfInterest:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsPointsOfInterest = YES;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)hideMapPointsOfInterest:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsPointsOfInterest = NO;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)showMapBuildings:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsBuildings = YES;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)hideMapBuildings:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsBuildings = NO;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)showMapTraffic:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsTraffic = YES;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+- (void)hideMapTraffic:(CDVInvokedUrlCommand*)command
+{
+    NSString* callbackId = [command callbackId];
+    NSString* mapId = [[command arguments] objectAtIndex:0];
+    MKMapView* mapView = [self.webView viewWithTag:mapId];
+
+    mapView.showsTraffic = NO;
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:mapId];
+
+    [self success:result callbackId:callbackId];
+
+}
+
+
+
+
 
 @end
