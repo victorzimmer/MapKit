35a885bf1bd32946dbe2f3935d3175539ff070d9
Victor Zimmer <victor.zimmer@me.com>
2015-11-13 09:33:37 +0100

    JS Added Global Reference to internal objects

--- www/mapkit.js
+++ www/mapkit.js
@@ -452,11 +452,13 @@ var MKMap = function (mapId) {
 
 function handlePinInfoClickCallback(title)
 {
-  Pins[title].infoClickCallback(Pins[title])
+  MKInterface.__internal__.Pins[title].infoClickCallback(Pins[title])
 }
 
 
 window.MKInterface = {}
+window.MKInterface.__internal__.Pins = Pins
+window.MKInterface.__internal__.PinsArray = PinsArray
 window.MKInterface.MKMap = MKMap
 window.MKInterface.locationManager = locationManager
 window.MKInterface.getMapByArrayId = function (aid) { return MapArray[aid] }
